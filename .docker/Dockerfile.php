7c8c97f46e19d73a7050b148dd054cef0bc57cd0
Simon Hobbs <simon@lilengine.co>
2019-10-24 16:41:20 +1100

    GOVCMS-3718: Env testing (#16)
    * GOVCMS-3718: Enable setting an image version, and dockerfiles that can use it

--- .docker/Dockerfile.php
+++ .docker/Dockerfile.php
@@ -1,6 +1,7 @@
 ARG CLI_IMAGE
-FROM ${CLI_IMAGE} as cli
+ARG GOVCMS_IMAGE_VERSION=latest
 
-FROM govcms8lagoon/php
+FROM ${CLI_IMAGE} as cli
+FROM govcms8lagoon/php:${GOVCMS_IMAGE_VERSION}
 
 COPY --from=cli /app /app
