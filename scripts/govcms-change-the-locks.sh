4b44f59e0831d3b0ab322d13db90c1d85857f954
sime <simon@hobbs.id.au>
2019-12-06 15:57:05 +1100

    Processing scripts updates.

--- scripts/govcms-change-the-locks.sh
+++ scripts/govcms-change-the-locks.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
 
-# This changes the locks from old saas to new saas.
+ACTION=${1:-"missing"}
+if [[ ${ACTION} = "missing" ]] ; then
+  echo "Pass a lock or unlock"
+  exit 1
+fi
 
+# Saas lock
+if [[ ${ACTION} = "lock" ]] ; then
+  git lfs lock composer.json
+  git lfs lock .gitlab-ci.yml
+  git lfs lock docker-compose.yml
+  git lfs lock .ahoy.yml
+  git lfs lock .env.default
+  exit 0
+fi
+
+# All unlock
+if [[ ${ACTION} = "unlock" ]] ; then
+  git lfs unlock .ahoy.yml
+  git lfs unlock .env.default
+  git lfs unlock .gitlab-ci.yml
+  git lfs unlock .lagoon.yml
+  git lfs unlock .version.yml
+  git lfs unlock README.md
+  git lfs unlock docker-compose.yml
+  exit 0
+fi
--- scripts/govcms-reconstruct-saas.sh
+++ scripts/govcms-reconstruct-saas.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
+IFS=$'\n\t'
+set -euo pipefail
 
-# This replicates the process of AWX applying a scaffold update.
+##
+# This demonstrates the process of AWX applying a scaffold update.
+#
 
-# 1. do you need to run "change-the-locks.
-# 2. clone the scaffold repo.
-# 3. recursive copy everything except .lagoon.yml, .env , .git
-# 4. Move themes directory and files directory
-# 5. Set the scaffold version to 1.x
-# 6. Commit to new branch (and push, if lfs unlocked).
+TMP_DIR=${TMPDIR:-/tmp/}
+CUR_DIR=$(pwd)
 
+SCAFFOLD_REPO=${SCAFFOLD_REPO:-https://github.com/simesy/govcms8-scaffold}
+SCAFFOLD_BRANCH=${SCAFFOLD_BRANCH:-paas-saas-mash-up}
+SCAFFOLD_DIR=${TMP_DIR}govmcs8-scaffold
+TARGET_DIR=${TARGET_DIR:-$CUR_DIR}
 
+# Prep scaffold source.
+rm -Rf ${TMP_DIR}/govmcs8-scaffold
+git clone --depth 1 ${SCAFFOLD_REPO} -b ${SCAFFOLD_BRANCH} ${SCAFFOLD_DIR}
+
+# Scaffold update.
+rsync -rl --stats --exclude ".git/" --exclude ".env" --exclude ".lagoon.yml" ${SCAFFOLD_DIR}/ ${TARGET_DIR}
+
+# Move themes.
+mv ${TARGET_DIR}/themes/* ${TARGET_DIR}/web/themes/custom/
+rm -Rf themes
+
+#
+cd ${TARGET_DIR}
+git add themes && git add web/themes
+# git commit -m"SaaS Scaffold Update: Moved themes to standard location."
+
+git add custom && git add drush && git add 
+# git commit -m"SaaS Scaffold Update: Moved themes to standard location."
+
+git add .
+# git commit -m"SaaS Scaffold Update: Update scaffold files."
